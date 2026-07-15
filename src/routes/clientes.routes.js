e8200cb0ce32a58662a1a140184e2c000756e85c
nessa112358 <brenda.pola.03@gmail.com>
2026-07-15 01:37:38 -0500

    modificar ruta de clientes

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { getClienteByCelular, registerCliente, loginCliente, upsertCliente } = require('../controllers/clientes.controller');
+const { getClienteByCelular, registerCliente, loginCliente, upsertCliente, syncGoogleCliente  } = require('../controllers/clientes.controller');
 
 const router = Router();
 
 router.post('/login', loginCliente);
 router.post('/register', registerCliente);
+router.post('/sync-google', syncGoogleCliente);
 router.post('/', upsertCliente);
 router.get('/:celular', getClienteByCelular);
 router.post('/sync-google', syncGoogleCliente);
